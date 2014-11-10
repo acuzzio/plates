89809ff7de611784476fc91675279dad35697e25
acuzzio <acuzzio@gmail.com>
2014-11-10 19:35:13 +0100

    added Z type plates

--- src/Main.hs
+++ src/Main.hs
@@ -10,36 +10,55 @@ import VerbatimParser
 main = do
   args <- getArgs
   case length args of
-       4 -> do
-            let  [x1,x2,dimF,totCharge] = map read2 args
-                 dim = floor dimF
-            piastrami x1 x2 dim totCharge
+       5 -> do
+            let  [plateT', x1', x2', dim', totCharge'] = args
+                 plateT    = read plateT' :: PlateType
+                 x1        = read2 x1'
+                 x2        = read2 x2'
+                 dim       = floor $ read2 dim'
+                 totCharge = read2 totCharge'
+            piastrami plateT x1 x2 dim totCharge
        otherwise -> putStrLn $ printVerbatim errMessage 
 
 errMessage = [verbatim|
 Please write:
-$ Plates x1 x2 radius charge
-$ Plates -5.0 6.0 15.0 1.0
+$ Plates plateType x1 x2 radius charge
+$ Plates X -5.0 6.0 15.0 1.0
+
+avaiable plateTypes:
+X - 2 circular plates in X axis
+Z - 2 circular plates in Z axis
+
 |]
 
-piastrami :: Double -> Double -> Integer -> Double -> IO()
-piastrami x x2 dim totalCharge = do
-  let singleCharge = totalCharge / (fromIntegral pointsN :: Double)
-      points       = [[x, y, z, singleCharge, 0.0, 0.0, 0.0] | y <- map (\x -> fromInteger x :: Double) [(-dim)..dim], z <- map (\x -> fromInteger x :: Double) [(-dim)..dim], lengthInferior y z (fromInteger dim :: Double)]
-      points2      = [[x2, y, z, (-singleCharge), 0.0, 0.0, 0.0] | y <- map (\x -> fromInteger x :: Double) [(-dim)..dim], z <- map (\x -> fromInteger x :: Double) [(-dim)..dim], lengthInferior y z (fromInteger dim :: Double)]
-      charges      = points ++ points2
+data PlateType = X | Z deriving (Show, Read)
+
+piastrami :: PlateType -> Double -> Double -> Integer -> Double -> IO()
+piastrami pt x x2 dim totalCharge = do
+  let charges      = generatePoints pt x x2 dim totalCharge 
       strings      = map (map show) charges
       reformatZ    = map (replace 3) strings 
       toFile       = unlines $ map unwords $ reformatZ
-      pointsN      = length points
       howMany      = show $ length charges
       toSee        = unlines $ map unwords $ map (\x -> ["H",x!!0,x!!1,x!!2]) strings
-  putStrLn $ howMany ++ "\n"
-  putStrLn toFile
+  electricField (abs (x2 - x)) dim
+  dypoleMoment totalCharge x x2
   writeFile "piastra" $ howMany ++ "\n" 
   appendFile "piastra" $ toFile
   writeFile "toSee.xyz" $ howMany ++ "\n\n"
   appendFile "toSee.xyz" $ toSee
+
+generatePoints pt x x2 dim totalCharge = case pt of
+  X -> let singleCharge = totalCharge / (fromIntegral pointsN :: Double)
+           points       = [[x, y, z, singleCharge, 0.0, 0.0, 0.0] | y <- map (\x -> fromInteger x :: Double) [(-dim)..dim], z <- map (\x -> fromInteger x :: Double) [(-dim)..dim], lengthInferior y z (fromInteger dim :: Double)]
+           points2      = [[x2, y, z, (-singleCharge), 0.0, 0.0, 0.0] | y <- map (\x -> fromInteger x :: Double) [(-dim)..dim], z <- map (\x -> fromInteger x :: Double) [(-dim)..dim], lengthInferior y z (fromInteger dim :: Double)]
+           pointsN      = length points
+       in points ++ points2  
+  Z -> let singleCharge = totalCharge / (fromIntegral pointsN :: Double)
+           points       = [[z, y, x, singleCharge, 0.0, 0.0, 0.0] | y <- map (\x -> fromInteger x :: Double) [(-dim)..dim], z <- map (\x -> fromInteger x :: Double) [(-dim)..dim], lengthInferior y z (fromInteger dim :: Double)]            
+           points2      = [[z, y, x2, (-singleCharge), 0.0, 0.0, 0.0] | y <- map (\x -> fromInteger x :: Double) [(-dim)..dim], z <- map (\x -> fromInteger x :: Double) [(-dim)..dim], lengthInferior y z (fromInteger dim :: Double)]
+           pointsN      = length points
+       in points ++ points2 
 
 
 reformat :: String -> String
@@ -57,7 +76,7 @@ lengthInferior :: Double -> Double -> Double -> Bool
 lengthInferior y z ray = let length = sqrt (y**2 + z**2)
                          in if length > ray then False else True
 
-electricField :: Double -> Int -> IO()
+electricField :: Double -> Integer -> IO()
 electricField x dimension = do
   let mToAng       = 1.0e-10
       elecCharge   = 1.6e-19
